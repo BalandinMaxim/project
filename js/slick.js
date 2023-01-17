fe3d58bde85b99e45ee7b5cd7c8f2f5230e5503d
BalandinMaxim <112868629+BalandinMaxim@users.noreply.github.com>
2023-01-17 13:32:23 +0300

    Update slick.js

--- js/slick.js
+++ js/slick.js
@@ -10,104 +10,157 @@ var g1;
 var window;
 var ResizeSensor;
 var jQuery;
+/*110rightx45bottom*/
 
-$("#slick-views-testimonials-block-1-1").on(
-  "init reInit afterChange",
-  function (event, slick, currentSlide, nextSlide) {
-    var i = (currentSlide ? currentSlide : 0) + 1;
-    $(this)
-      .find(".slick-slide-num")
-      .html(
-        '<span class="slick-slide-num-current">' +
-          i +
-          "</span> / " +
-          slick.slideCount
-      );
-  }
-);
 
-$("#slick-views-testimonials-block-1-1-slider").slick({
-  adaptiveHeight: true,
-  centerMode: false,
-  fade: true,
-  arrows: true,
-  prevArrow: $("slprev"),
-  nextArrow: $("slnext"),
-});
+function Admin_menu_on() {
+    $("#Admin-menu").show(300);
+}
 
-$(".slprev").on("click", function () {
-  $("#slick-views-testimonials-block-1-1-slider").slick("slickPrev");
-});
-$(".slnext").on("click", function () {
-  $("#slick-views-testimonials-block-1-1-slider").slick("slickNext");
-});
+function Admin_menu_leave() {
+    $("#Admin-menu").hide(300);
+}
 
-$("#slick-views-our-clients-block-1-2-slider").slick({
-  mobileFirst: false,
-  autoplay: true,
-  infinite: true,
-  pauseOnHover: false,
-  arrows: false,
-  slidesToShow: 5,
-  centerMode: true,
-  centerPadding: "10%",
-  autoplaySpeed: 2000,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        autoplay: true,
-        pauseOnHover: false,
+function About_menu_on() {
+    $("#About-menu").show(300);
+}
+
+function About_menu_leave() {
+    $("#About-menu").hide(300);
+}
+
+function Mobile_menu() {
+    var cmm = $("#click-menu-mobile");
+    if (cmm.css("display") === "none") {
+        cmm.show(300);
+    } else {
+        cmm.hide(300);
+    }
+}
+
+function Admin_mobile() {
+    var admm = $("#Admin-menu-mobile");
+    if (admm.css("display") === "none") {
+        admm.show(300);
+    } else {
+        admm.hide(300);
+    }
+}
+
+function About_mobile() {
+    var abmm = $("#About-menu-mobile");
+    if (abmm.css("display") === "none") {
+        abmm.show(300);
+    } else {
+        abmm.hide(300);
+    }
+}
+
+$(document).ready(function () {  //Данная функция будет выполняться после загрузки всей сраницы.
+    var c;
+    var a = 0;
+    $("#Reviews").css("padding-bottom", $("#Com_1").height());   //Перерасчитываем размер блока с отзываеми при помощи библиотеки найденной в интернете.
+    $(window).resize(function () {
+        new ResizeSensor(jQuery("#Com_1"), function () {
+            $("#Reviews").css("padding-bottom", $("#Com_1").height());
+        });
+    });
+    $("#faq_id").click(function () {
+        a = a + 1;
+        if (a % 2 !== 0) {
+            $("#faq_id").css("color", "rgb(241, 77,52)");
+            $("#one").css("border", "3px solid rgb(241, 77,52)");
+        } else {
+            $("#faq_id").css("color", "black");
+            $("#one").css("border", "0px");
+        }
+    });
+
+    $("#slider_one").slick({   //создаем первый слайдер
+        accessibility: false,
         arrows: false,
-        slidesToShow: 3,
-        waitForAnimate: false,
-      },
-    },
-    {
-      breakpoint: 600,
-      settings: {
         autoplay: true,
-        pauseOnHover: false,
+        autoplaySpeed: 3000,
+        dots: false,
+        slidesToScroll: 1,
+        slidesToShow: 6,
+        variableWidth: true
+    });
+    $("#slider_two").slick({   //создаем второй слайдер
+        accessibility: false,
         arrows: false,
-        slidesToShow: 2,
-        waitForAnimate: false,
-      },
-    },
-  ],
-});
+        autoplay: true,
+        autoplaySpeed: 3000,
+        centerMode: true,
+        dots: false,
+        slidesToScroll: 1,
+        slidesToShow: 6,
+        variableWidth: true
+    });
+    $("#slider_three").slick({  //создаем третий слайдер
+        arrows: true,
+        dots: false,
+        nextArrow: $("#next"),
+        prevArrow: $("#prev"),
+        slidesToScroll: 1,
+        slidesToShow: 1
+    });
+    $("#slider_three").on("afterChange", function (event, slick, currentSlide) {  //Это 3d слайдер, реализацию которого мы нашли в интернете. 
+        g = event.type;
+        g1 = slick.type;
+        $("#numb").text("0" + (currentSlide + 1));
+    });
+    $num = $(".ui-card").length;
+    $even = $num / 2;
+    $odd = ($num + 1) / 2;
+    if ($num % 2 === 0) {
+        $(".ui-card:nth-child(" + $even + ")").addClass("active");
+        $(".ui-card:nth-child(" + $even + ")").prev().addClass("prev");
+        $(".ui-card:nth-child(" + $even + ")").next().addClass("next");
+    } else {
+        $(".ui-card:nth-child(" + $odd + ")").addClass("active");
+        $(".ui-card:nth-child(" + $odd + ")").prev().addClass("prev");
+        $(".ui-card:nth-child(" + $odd + ")").next().addClass("next");
+    }
+    c = 1;
+    $(".ui-card").click(function () {
+        $slide = $(".active").width();
+        if ($(this).hasClass("next")) {
+            $(".container").stop(false, true).animate({left: "-=" + $slide});
+            c += 1;
+        } else if ($(this).hasClass("prev")) {
+            $(".container").stop(false, true).animate({left: "+=" + $slide});
+            c -= 1;
+        }
+        if (c === 0) {
+            $("#but_1").css("background", "rgb(241, 77,52)");
+            $("#but_1").css("color", "white");
+            $("#but_2").css("color", "rgb(241, 77,52)");
+            $("#but_2").css("background", "white");
+            $("#but_3").css("color", "rgb(241, 77,52)");
+            $("#but_3").css("background", "white");
+        } else if (c === 1) {
+            $("#but_2").css("background", "rgb(241, 77,52)");
+            $("#but_2").css("color", "white");
+            $("#but_1").css("color", "rgb(241, 77,52)");
+            $("#but_1").css("background", "white");
+            $("#but_3").css("color", "rgb(241, 77,52)");
+            $("#but_3").css("background", "white");
+        } else if (c === 2) {
+            $("#but_3").css("background", "rgb(241, 77,52)");
+            $("#but_3").css("color", "white");
+            $("#but_2").css("color", "rgb(241, 77,52)");
+            $("#but_2").css("background", "white");
+            $("#but_1").css("color", "rgb(241, 77,52)");
+            $("#but_1").css("background", "white");
+        }
 
-$("#slick-views-our-clients-attachment-1-3-slider").slick({
-  mobileFirst: false,
-  autoplay: true,
-  infinite: true,
-  pauseOnHover: false,
-  arrows: false,
-  slidesToShow: 5,
-  centerMode: true,
-  centerPadding: "10%",
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        autoplay: true,
-        pauseOnHover: false,
-        arrows: false,
-        slidesToShow: 3,
-        waitForAnimate: false,
-      },
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        autoplay: true,
-        pauseOnHover: false,
-        arrows: false,
-        slidesToShow: 2,
-        waitForAnimate: false,
-      },
-    },
-  ],
-});
+        $(this).removeClass("prev next");
+        $(this).siblings().removeClass("prev active next");
+        $(this).addClass("active");
+        $(this).prev().addClass("prev");
+        $(this).next().addClass("next");
+    });
 const app = new Vue({   //Это переменная формы, которая отображается при нажатии на кнопку связатьяс с нами.
         el: '#app',   //id этой формы
         data: {   //поля формы
